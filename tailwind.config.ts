da6605329fcef0815c3825b6f9484fed7ace38c4
nguyentanphatt <bloodomega97@gmail.com>
2025-02-17 16:11:11 +0700

    feat: bookdetail

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import {heroui} from '@heroui/theme';
 import type { Config } from "tailwindcss";
 
 export default {
@@ -5,14 +6,25 @@ export default {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/components/(breadcrumbs|table|checkbox|form|spacer).js",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        "blue": "#0B3D91"
+      },
+      fontFamily: {
+        merriweather: "var(--font-merriweather), serif",
       },
     },
+    screens: {
+  		sm: '375px',
+  		md: '768px',
+  		lg: '1440px',
+  	}
   },
-  plugins: [],
+  plugins: [heroui()],
 } satisfies Config;
